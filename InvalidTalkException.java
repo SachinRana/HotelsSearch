6cfa855f0e8f132d5c0dd211bd579517a7d049bb
SachinRana <sachin.java123@yahoo.com>
2014-12-18 00:33:29 +0530

    InvalidTalkException.java
    Exception class

---
+++ InvalidTalkException.java
@@ -0,0 +1,11 @@
+package Flie;
+
+public class InvalidTalkException extends Exception{
+   
+    private static final long serialVersionUID = 1L;
+
+    public InvalidTalkException(String msg) {
+        super(msg);
+    }
+
+}
